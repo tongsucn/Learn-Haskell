40fbabf04cc7f4015d6a1d1b79ed4663df327926
Tong Su <tongsu.cn@gmail.com>
2016-05-23 17:48:19 +0200

    Update solution to 26.hs, add solution to 27.hs.

--- H-99/27.hs
+++ H-99/27.hs
@@ -1,12 +1,20 @@
-module H99_26
+module H99_27
 where
 
 combinations :: Int -> [a] -> [[a]]
 combinations num lst
   | num < 0 || num > (length lst) = error "Out of range!"
   | num == 0 = [[]]
+  | (length lst) == 0 = [[]]
   | otherwise = concat [map (joinHead idx) (genTail idx) | idx <- candIdxLst]
     where
       joinHead idx = (\tailLst -> ((lst !! idx):tailLst))
       genTail idx = combinations (num - 1) (drop (idx + 1) lst)
       candIdxLst = [0..(length lst - num)]
+
+group :: [Int] -> [a] -> [[[a]]]
+group pat lst
+  | not null (filter (\x -> x < 0) lst) = error "Pattern contain negative!"
+  | (sum pat) /= (length lst) = error "Format error!"
+  | null lst = [[[]]]
+  | otherwise = 
